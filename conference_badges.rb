83e16679794cc8a6bcb858a47b04ea1f42a039b8
Joseph Vint <josephvint@me.com>
2018-02-10 20:00:36 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,7 +11,7 @@ def batch_badge_creator(speakers)
 end
 
 def assign_rooms(speakers)
-  speakers.collect.index do |name,index|
+  speakers.collect.each_with_index do |name,index|
     "Hello, #{name}! You'll be assigned to room #{index + 1}!"
   end
 end
